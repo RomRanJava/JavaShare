b16fcff40065f243194488b5b18f4e2f84fc3bbc
RAN <RAN@Lenovo-PC>
2015-11-11 20:07:32 +0200

    rom suck my dick!!!!!

--- src/com/myorg/javacourse/LearnServlet.java
+++ src/com/myorg/javacourse/LearnServlet.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class RomAndRanServlet extends HttpServlet {
+public class LearnServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, yyworld");
+		resp.getWriter().println("Hello, ran");
 	}
 }
