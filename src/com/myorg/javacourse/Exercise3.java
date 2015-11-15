676e20eec3884dc3410c38e3455ce7c07491872d
Rom Vahav <Rom Vahav@DESKTOP-8DBJNAK>
2015-11-15 12:36:32 +0200

    Ran shamen maniak !!!!

---
+++ src/com/myorg/javacourse/Exercise3.java
@@ -0,0 +1,35 @@
+package com.myorg.javacourse;
+
+import java.io.IOException;
+//import java.Math;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Exercise3 extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		double radius = 50;
+		double area;
+		
+		area = Math.PI * Math.pow(radius, 2);
+		
+		resp.getWriter().println("1. Area of circle with radius " + radius + " is: " + area + " square­cm");
+		
+		double angleB = 30;
+		double hypotenuse = 50;
+		double opposite;
+		double radian;
+		
+		radian = Math.toRadians(angleB);
+		opposite = Math.sin(radian) * hypotenuse;
+		resp.getWriter().println("2. Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: " + opposite);
+		
+		double base = 20;
+		double exp = 13;
+		double result;
+		
+		result = Math.pow(base, exp);
+		resp.getWriter().println("3. Power of 20 with exp of 13 is: " + result);
+		
+	}
+}
